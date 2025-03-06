6a773ce7ec4943900d2d358ec44d9ed964fc0c8e
Alireza Ansari <alireza.asr98@gmail.com>
2025-03-06 12:18:11 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,68 +1,54 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <string.h>  
+#include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_log.h"
-#include "esp_wifi.h"
-#include "nvs_flash.h"
 #include "esp_event.h"
-#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "wifi_provisioning/manager.h"
+#include "wifi_provisioning/scheme_softap.h"
 
-static const char *TAG = "wifi_example";
+#define WIFI_SSID "TestAP"
+#define WIFI_PASSWORD "Password"  
 
-esp_err_t wifi_event_handler(void *ctx, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    switch (event_id) {
-        case WIFI_EVENT_STA_START:
-            ESP_LOGI(TAG, "Station started");
-            esp_wifi_connect();
-            break;
-        case WIFI_EVENT_STA_CONNECTED:
-            ESP_LOGI(TAG, "Connected to WiFi");
-            break;
-        case IP_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "Got IP");
-            break;
-        case WIFI_EVENT_STA_DISCONNECTED:
-            ESP_LOGI(TAG, "Disconnected from WiFi, retrying...");
-            esp_wifi_connect();
-            break;
-        default:
-            break;
-    }
-    return ESP_OK;
-}
+static const char *TAG = "wifi softAP";
 
-void wifi_init_sta() {
+void wifi_init_softap(void)
+{
     esp_log_level_set("*", ESP_LOG_INFO);
-    ESP_ERROR_CHECK(nvs_flash_init());
+    esp_log_level_set(TAG, ESP_LOG_INFO);
 
-    esp_netif_init();
-    esp_event_loop_create_default();
-    
-    esp_netif_t *netif = esp_netif_create_default_wifi_sta();
+    ESP_ERROR_CHECK( esp_netif_init() );
+    ESP_ERROR_CHECK( esp_event_loop_create_default() );
+
+    esp_netif_create_default_wifi_ap();
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-    
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL);
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
 
     wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "SSID",  
-            .password = "PASSWD@",  
-            .bssid_set = false,
+        .ap = {
+            .ssid = WIFI_SSID,
+            .ssid_len = strlen(WIFI_SSID),
+            .password = WIFI_PASSWORD,
+            .max_connection = 4,
+            .authmode = WIFI_AUTH_WPA2_PSK,
         },
     };
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
-    esp_wifi_start();
+
+    if (strlen(WIFI_PASSWORD) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_AP) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_AP, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+
+    ESP_LOGI(TAG, "Access Point '%s' started with password '%s'", WIFI_SSID, WIFI_PASSWORD);
 }
 
-void app_main(void) {
-    ESP_LOGI(TAG, "Initializing WiFi...");
-    wifi_init_sta();
-    
-    while (true) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  
-    }
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    wifi_init_softap();
 }
